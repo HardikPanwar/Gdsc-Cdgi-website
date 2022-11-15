58a67ec5cae1c1dc1b9375037eb663ef0a0ee9ba
Priyanshu joshi <prjoshi2710@gmail.com>
2022-11-15 12:07:40 +0530

    priyanshu first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar.js'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './Components/Home';
+import Contactus from './Components/Contactus';
+import About from './Components/About';
+import Events from './Components/Events';
+import Teams from './Components/Teams';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+  <BrowserRouter>
+  <Navbar/>
+  <Routes>
+  <Route path='/' element={<Home/>} />
+  <Route path='contact' element={<Contactus/>} />
+  <Route path='about' element={<About/>} />
+  <Route path='event' element={<Events/>} />
+  <Route path='teams' element={<Teams/>} />
+  
+</Routes>
+  </BrowserRouter>   
+
+     
     </div>
   );
 }
